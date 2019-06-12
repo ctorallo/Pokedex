6ea6022039ed2236d18f35bfc49f814000567635
Duds <ccdtorallo@gmail.com>
2019-06-12 10:30:46 +0800

    reworked some routing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
-import { BoxResultComponent } from './pokemon-list/box-result/box-result.component';
 import { DetailBoxComponent } from './detail-box/detail-box.component';
 import { AppGuard } from './app.guard';
 
 const routes: Routes = [
-    { path: '', component: PokemonListComponent},
-    { path: ':details', component: DetailBoxComponent,
-              canActivate: [AppGuard], runGuardsAndResolvers: 'always'},
-    { path: '**', redirectTo: ':details' },
+    { path: '', redirectTo: '/list', pathMatch: 'full' },
+    { path: 'list', component: PokemonListComponent },
+    { path: 'list/:details', component: DetailBoxComponent },
+    { path: '**', redirectTo: ':list' }
+    
+    
+    // { path: ':details', component: DetailBoxComponent,
+    //           canActivate: [AppGuard], runGuardsAndResolvers: 'always'},
+    // { path: '**', redirectTo: ':details' },
 
 ];
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes)],
-  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
+  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload',enableTracing: false})],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
